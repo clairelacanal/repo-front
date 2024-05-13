407279cc9b50a36d8a67f9e452ac135ad7c4a8e1
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-05-13 11:20:50 +0200

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-
-import './App.css'
-import NotFoundPage from './pages/NotFoundPage';
-import AboutPage from './pages/AboutPage';
-import Homepage from './Components/HOMEPAGE/Home';
-import { Route, Routes } from 'react-router-dom';
-
+import "./App.css";
+import NotFoundPage from "./Pages/NotFoundPage";
+import AboutPage from "./Pages/AboutPage";
+//import Homepage from "./Components/HOMEPAGE/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  
-    <> 
+  <>
     <div>
-    
       <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-      <Route path='*' element={<NotFoundPage/>}/>
-
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    
     </div>
-
-
-    </>
-  )
+  </>;
 }
 
-export default App
+export default App;
